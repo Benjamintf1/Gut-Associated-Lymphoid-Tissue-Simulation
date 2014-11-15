e3f036d5da922049f26df6976afe8fc7bd292045
Benjamintf1 <Benjamintf1@gmail.com>
2014-11-15 14:17:48 -0700

    Added the generator code

---
+++ generate_tivb_files.cpp
@@ -0,0 +1,62 @@
+#include <fstream>  //input/output from files
+#include "generators.h"
+
+using namespace std;
+
+int main(int argc, char* argv){
+
+	
+	if(argc < 2){
+		printf("You must give the program a configuration file. Check the readme for how the config file should be formatted");
+		exit(1); // we cannot continue without the config
+	}
+
+
+
+	ifstream config_file(argv[1]);
+
+	
+	double delta_space; //The spatial step length (evenly sized in x and y)
+	int grid_width; //The number of x steps
+	int grid_height; //The number of y steps
+	string birth_rate_filename;
+	string t_filename;
+	string i_filename;
+	string v_filename;
+
+	config_file >> delta_space;
+	config_file >> grid_width;
+	config_file >> grid_height;
+	config_file >> birth_rate_filename;
+	config_file >> t_filename;
+	config_file >> i_filename;
+	config_file >> v_filename;
+	
+	config_file.close();
+
+
+	ofstream generated_t_file(t_filename.c_str(), ios::out |ios::binary);
+	ofstream generated_i_file(i_filename.c_str(), ios::out |ios::binary);
+	ofstream generated_v_file(v_filename.c_str(), ios::out |ios::binary);
+	ofstream generated_birth_file(birth_rate_filename.c_str(), ios::out |ios::binary);
+
+
+	for(int i = 0; i < grid_height; ++i){
+		for(int j = 0; j < grid_width; ++j){
+			binary_write(generated_t_file , generate_T(i, j, grid_height, grid_width, delta_space));
+			binary_write(generated_i_file , generate_I(i, j, grid_height, grid_width, delta_space));
+			binary_write(generated_v_file , generate_V(i, j, grid_height, grid_width, delta_space));
+			binary_write(generated_birth_file, generate_birth(i, j, grid_height, grid_width, delta_space));
+		}
+	}
+
+
+	generated_t_file.close();
+	generated_i_file.close();
+	generated_v_file.close();
+	generated_birth_file.close();
+
+
+	return 0;
+}
+
---
+++ generators.h
@@ -0,0 +1,13 @@
+#include <string>
+
+double generate_T(int x, int y, int height, int width, double delta_space );
+
+double generate_I(int x, int y, int height, int width, double delta_space );
+
+
+double generate_V(int x, int y, int height, int width, double delta_space );
+
+double generate_birth(int x, int y, int height, int width, double delta_space );
+
+
+
