bbd417e303c4b571bc0ac12104d4b0194b685676
Nathan Neri <nneri@mymail.mines.edu>
2014-12-01 16:16:45 -0700

    Update generators.h to match feature change

--- src/generators.h
+++ src/generators.h
@@ -4,10 +4,11 @@ double generate_T(int x, int y, int width, int height, double delta_space );
 
 double generate_I(int x, int y, int width, int height, double delta_space );
 
-
 double generate_V(int x, int y, int width, int height, double delta_space );
 
 double generate_birth(int x, int y, int width, int height, double delta_space );
+
+int generate_tissues(int x, int y, int width, int height, double delta_space );
 
 
 
